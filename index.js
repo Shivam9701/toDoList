7728c1fb2795b374fed317099bbad15814ac773e
Shivam9701 <shivamkichitthi@gmail.com>
2023-08-22 15:47:05 +0530

    "v3"

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ import { fileURLToPath } from 'url';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import axios from 'axios';
+import SpotifyWebApi from 'spotify-web-api-node';
 import { type } from 'os';
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 dotenv.config();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
-const spotify_client_secret =process.env.SPOTIFY_CLIENT_SECRET;
-console.log("hi " +spotify_client_id);
+const spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const app = express();
 const port = 3000;
+const spotifyApi =new SpotifyWebApi();
 
 var generateRandomString = function (length) {
     var text = '';
@@ -23,22 +23,69 @@ var generateRandomString = function (length) {
     }
     return text;
 };
+
+function millisToMinutesAndSeconds(millis) {
+    var minutes = Math.floor(millis / 60000);
+    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', 'views');
 app.set('view engine', 'ejs');
-//const accessToken = "BQBx_JVcy1Xo7eysLhBTNsz-M475sBoZkbKtUkHzRQm_xfotYsnBapo-13EwbuasYsSWQ6qARHC55TbmW7iiEBb-faGkC_pJg1F0KPW7Dpd8JN3dnk8SeYVuM8H4eAjq7deweUKA8ObdcxlqZttDtg-8t1w1OvkrdqybSO_nQfrSub0HxEUg_2hZT5w8rENVCmPEvB25bswHsLuiX2iNPYhGa3dC";
+
 app.get('/', (req, res) => {
-    if (!req.query.valid){
-    console.log(req.body);
-    res.render('home');
-    }else{
-        console.log("name"+req.query.valid);
+
+    if (!req.query.valid) {
         res.render('home');
+    } 
+    else {
+        console.log("authorized! valid : " + req.query.valid);
+        res.render('index');
     }
+
 });
 
-let tasksList = [];
+
+app.post('/', async(req, res) => {
+
+    const task = req.body.task;
+    //console.log(task);
+    try{
+    const response = await spotifyApi.searchTracks(task,{limit:5});
+    console.log(response.body);
+    const listTasks = response.body.tracks.items;
+    let songsDisplay=[];
+    
+
+    listTasks.forEach(track =>{
+        let song = { name: "", duration: "", artists: [], img: "" };
+        song.name =track.name;
+        console.log(song.name);
+        song.duration = millisToMinutesAndSeconds(track.duration_ms);
+        console.log(song.duration);
+
+        for (let i = 0; i<track.artists.length ;i++){
+            song.artists[i]=track.artists[i].name;
+            console.log(song.artists[i]);
+        }
+        song.img=track.album.images[0].url;
+        console.log(song.img);
+        songsDisplay.push(song);
+    });
+    res.locals.taskString=`Showing top 5 results for '${task}'`;
+    res.locals.songsDisplay=songsDisplay;
+    res.status(200).render('index');
+
+    }
+    catch(err){
+        console.error(err);
+        res.status(404).send(err);
+    }
+    
+
+});
 
 app.get('/auth/login', (req, res) => {
 
@@ -62,7 +109,7 @@ app.get('/auth/login', (req, res) => {
 });
 app.get('/auth/callback', async (req, res) => {
     const code = req.query.code;
-    const url ='https://accounts.spotify.com/api/token';
+    const url = 'https://accounts.spotify.com/api/token';
     const redirectUri = "http://localhost:3000/auth/callback";
 
     const body = {
@@ -70,55 +117,37 @@ app.get('/auth/callback', async (req, res) => {
         code: code,
         redirect_uri: redirectUri,
     };
-    const config ={
-        headers : {
+    const config = {
+        headers: {
             'Authorization': 'Basic ' + (Buffer.from(spotify_client_id + ':' + spotify_client_secret).toString('base64')),
             'Content-Type': 'application/x-www-form-urlencoded',
-    },
-};
+        },
+    };
     try {
-        const response =await axios.post(url,body,config);
+        const response = await axios.post(url, body, config);
         //console.log(response.data);
         var accessToken = response.data.access_token;
-        const rep = await axios.get(
-            "https://api.spotify.com/v1/tracks/4JvziZ2kQsbzfvRQtpYfmJ",
-            {
-                headers: {
-                    Authorization: "Bearer " + accessToken ,
-                },
-            }
-        );
-        const data =rep.data;
-        console.log("gand\n");
-        console.log(data.album);
-        
-        
-        var string = encodeURIComponent(data.album.name);
-        res.redirect('/?valid='+string);
-        
+        // const rep = await axios.get(
+        //     "https://api.spotify.com/v1/tracks/4JvziZ2kQsbzfvRQtpYfmJ",
+        //     {
+        //         headers: {
+        //             Authorization: "Bearer " + accessToken,
+        //         },
+        //     }
+        // );
+        // const data = rep.data;       
+        // var string = encodeURIComponent(data.album.name);
+        spotifyApi.setAccessToken(accessToken);
+        var string="start";
+        res.redirect('/?valid=' + string);
+
     } catch (error) {
         console.error(error);
         res.status(500).send('error');
     }
 
 });
-app.get('/play',(req, res) => {
-    
-})
 
-app.post('/submit', (req, res) => {
-    
-    const task = req.body.task;
-    console.log(task);
-
-    if (task) {
-        tasksList.push(task);
-    }
-    res.locals.listTasks = tasksList;
-    req.body.task=null;
-    res.render('index');
-
-});
 
 app.listen(port, () => {
     console.log('listening on port');
